4e4c12b33693fbcd57f3fcd73354dfc53a2e2d3a
wolf <wolf wolf@ewell.finance>
2024-01-24 21:46:12 +0800

    add use ValidTokenBalance

--- contract/Ewell.Contracts.Ido/EwellContract.cs
+++ contract/Ewell.Contracts.Ido/EwellContract.cs
@@ -46,7 +46,9 @@ namespace Ewell.Contracts.Ido
             var virtualAddressHash = GetProjectVirtualAddressHash(Context.Sender); 
             var virtualAddress = Context.ConvertVirtualAddressToContractAddress(virtualAddressHash);
             
-            TransferFrom(virtualAddressHash, Context.Sender, virtualAddress, input.ProjectCurrency, input.CrowdFundingIssueAmount);
+            //TransferFrom(virtualAddressHash, Context.Sender, virtualAddress, input.ProjectCurrency, input.CrowdFundingIssueAmount);
+            ValidTokenBalance(input.ProjectCurrency, virtualAddress, input.CrowdFundingIssueAmount);
+
             State.ProjectAddressMap[id] = virtualAddress;
             State.ProjectCreatorIndexMap[Context.Sender] = State.ProjectCreatorIndexMap[Context.Sender].Add(1);
 
